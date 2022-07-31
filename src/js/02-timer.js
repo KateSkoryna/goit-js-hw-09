566a5b560c56294236034a8165f4f1a11f218e3b
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-31 15:41:20 +0200

    Correction to Task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -36,19 +36,16 @@ flatpickr('#datetime-picker', options);
 const inputDate = document.querySelector('#datetime-picker')._flatpickr;
 
 startBtn.addEventListener('click', onClickStartCount);
+let timerId = null;
 
 function onClickStartCount(event) {
-  if (event.target.removeAttribute('disabled')) {
+  if (timerId > 0) {
     return;
   }
-
-  let timerId = null;
-
   timerId = setInterval(() => {
     startBtn.removeAttribute('disabled');
 
     const deltaTime = choosedDate - Date.now();
-    console.log(choosedDate);
     console.log(deltaTime);
 
     const { days, hours, minutes, seconds } = convertMs(deltaTime);
@@ -82,75 +79,3 @@ function convertMs(ms) {
 function padStart(value) {
   return String(value).padStart(2, '0');
 }
-
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-
-// const refs = {
-//   input: document.querySelector('#datetime-picker'),
-//   btnStart: document.querySelector('[data-start]'),
-//   timerValue: document.querySelector('.value'),
-// };
-
-// refs.btnStart.addEventListener('click', startTimer);
-
-// refs.btnStart.disabled = true;
-
-// let deadline = null;
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     deadline = selectedDates[0].getTime();
-//     const d = deadline - Date.now();
-//     console.log(deadline);
-//     if (d <= 0) {
-//       alert('Please choose a date in the future');
-//     }
-
-//     refs.btnStart.disabled = false;
-//     // return deadline
-//   },
-// };
-
-// const fp = flatpickr(refs.input, options);
-// console.log(deadline);
-
-// function initialTimer({ days, hours, minutes, seconds }) {
-//   refs.timerValue.textContent = `${days}:${hours}:${minutes}:${seconds}`;
-// }
-
-// function startTimer(e) {
-//   setInterval(() => {
-//     const timeValue = convertMs(deltaTime);
-//     console.log(timeValue);
-//   }, 1000);
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = addLeadingZero(Math.floor(ms / day));
-//   // Remaining hours
-//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-//   // Remaining minutes
-//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-//   // Remaining seconds
-//   const seconds = addLeadingZero(
-//     Math.floor((((ms % day) % hour) % minute) / second)
-//   );
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// function addLeadingZero(value) {
-//   return String(value).padStart(2, '0');
-// }

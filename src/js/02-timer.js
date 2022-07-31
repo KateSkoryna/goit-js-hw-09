8336468612573d5c8be01e0a41dd305a939b0aad
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-31 14:28:17 +0200

    Added Task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,102 +1,18 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-// const startBtn = document.querySelector('button[data-start]');
-
-// const DISABLE_CLASS = 'disabled';
-// let choosedDate = null;
-
-// const timer = {
-//   days: document.querySelector('span[data-days]'),
-//   hours: document.querySelector('span[data-hours]'),
-//   minutes: document.querySelector('span[data-minutes]'),
-//   seconds: document.querySelector('span[data-seconds]'),
-// };
-
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     choosedDate = selectedDates[0].getTime();
-
-//     const deltaDate = choosedDate - Date.now();
-//     if (deltaDate <= 0) {
-//       Notify.failure('Please choose a date in the future!');
-//       return;
-//     }
-//     startBtn.removeAttribute('disabled');
-//     return choosedDate;
-//   },
-// };
-
-// flatpickr('#datetime-picker', options);
-// const inputDate = document.querySelector('#datetime-picker')._flatpickr;
-
-// startBtn.addEventListener('click', onClickStartCount);
-
-// function onClickStartCount(event) {
-//   if (event.target.removeAttribute('disabled')) {
-//     return;
-//   }
-
-//   let timerId = null;
-
-//   timerId = setInterval(() => {
-//     startBtn.removeAttribute('disabled');
-
-//     const deltaTime = choosedDate - Date.now();
-//     console.log(choosedDate);
-//     console.log(deltaTime);
-
-//     const { days, hours, minutes, seconds } = convertMs(deltaTime);
-
-//     if (deltaTime >= 0) {
-//       timer.days.textContent = days;
-//       timer.hours.textContent = hours;
-//       timer.minutes.textContent = minutes;
-//       timer.seconds.textContent = seconds;
-//     } else {
-//       clearInterval(timerId);
-//     }
-//   }, 1000);
-// }
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = padStart(Math.floor(ms / day));
-//   const hours = padStart(Math.floor((ms % day) / hour));
-//   const minutes = padStart(Math.floor(((ms % day) % hour) / minute));
-//   const seconds = padStart(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
-// }
-
-// function padStart(value) {
-//   return String(value).padStart(2, '0');
-// }
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  input: document.querySelector('#datetime-picker'),
-  btnStart: document.querySelector('[data-start]'),
-  timerValue: document.querySelector('.value'),
+const startBtn = document.querySelector('button[data-start]');
+
+const DISABLE_CLASS = 'disabled';
+let choosedDate = null;
+
+const timer = {
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
 };
-
-refs.btnStart.addEventListener('click', startTimer);
-
-refs.btnStart.disabled = true;
-
-let deadline = null;
 
 const options = {
   enableTime: true,
@@ -104,29 +20,47 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    deadline = selectedDates[0].getTime();
-    const d = deadline - Date.now();
-    console.log(deadline);
-    if (d <= 0) {
-      alert('Please choose a date in the future');
-    }
+    choosedDate = selectedDates[0].getTime();
 
-    refs.btnStart.disabled = false;
-    // return deadline
+    const deltaDate = choosedDate - Date.now();
+    if (deltaDate <= 0) {
+      Notify.failure('Please choose a date in the future!');
+      return;
+    }
+    startBtn.removeAttribute('disabled');
+    return choosedDate;
   },
 };
 
-const fp = flatpickr(refs.input, options);
-console.log(deadline);
+flatpickr('#datetime-picker', options);
+const inputDate = document.querySelector('#datetime-picker')._flatpickr;
 
-function initialTimer({ days, hours, minutes, seconds }) {
-  refs.timerValue.textContent = `${days}:${hours}:${minutes}:${seconds}`;
-}
+startBtn.addEventListener('click', onClickStartCount);
 
-function startTimer(e) {
-  setInterval(() => {
-    const timeValue = convertMs(deltaTime);
-    console.log(timeValue);
+function onClickStartCount(event) {
+  if (event.target.removeAttribute('disabled')) {
+    return;
+  }
+
+  let timerId = null;
+
+  timerId = setInterval(() => {
+    startBtn.removeAttribute('disabled');
+
+    const deltaTime = choosedDate - Date.now();
+    console.log(choosedDate);
+    console.log(deltaTime);
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    if (deltaTime >= 0) {
+      timer.days.textContent = days;
+      timer.hours.textContent = hours;
+      timer.minutes.textContent = minutes;
+      timer.seconds.textContent = seconds;
+    } else {
+      clearInterval(timerId);
+    }
   }, 1000);
 }
 
@@ -137,20 +71,86 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
+  const days = padStart(Math.floor(ms / day));
+  const hours = padStart(Math.floor((ms % day) / hour));
+  const minutes = padStart(Math.floor(((ms % day) % hour) / minute));
+  const seconds = padStart(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
 
-function addLeadingZero(value) {
+function padStart(value) {
   return String(value).padStart(2, '0');
 }
+
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+
+// const refs = {
+//   input: document.querySelector('#datetime-picker'),
+//   btnStart: document.querySelector('[data-start]'),
+//   timerValue: document.querySelector('.value'),
+// };
+
+// refs.btnStart.addEventListener('click', startTimer);
+
+// refs.btnStart.disabled = true;
+
+// let deadline = null;
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     deadline = selectedDates[0].getTime();
+//     const d = deadline - Date.now();
+//     console.log(deadline);
+//     if (d <= 0) {
+//       alert('Please choose a date in the future');
+//     }
+
+//     refs.btnStart.disabled = false;
+//     // return deadline
+//   },
+// };
+
+// const fp = flatpickr(refs.input, options);
+// console.log(deadline);
+
+// function initialTimer({ days, hours, minutes, seconds }) {
+//   refs.timerValue.textContent = `${days}:${hours}:${minutes}:${seconds}`;
+// }
+
+// function startTimer(e) {
+//   setInterval(() => {
+//     const timeValue = convertMs(deltaTime);
+//     console.log(timeValue);
+//   }, 1000);
+// }
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', createPromisesOnSubmit);
+
+function createPromisesOnSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.target;
+
+  const deltaTime = parseInt(delay.value);
+  const stepTime = parseInt(step.value);
+  const amountNumber = parseInt(amount.value);
+
+  setTimeout(() => {
+    countPromises(amountNumber, stepTime);
+  }, deltaTime);
+}
+
+function countPromises(count, stepTime) {
+  let time = 0;
+
+  for (let i = 1; i <= count; i += 1) {
+    time += stepTime;
+    createPromise(i, time)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setInterval(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
